e78d36b6c783a12b2ecf9b6eaf84dfb54b3c8c11
Cas0n-McVey <absolgamer25@gmail.com>
2025-02-27 13:07:13 -0600

    Initial commit

---
+++ Variables_McVey_Cason/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Variables_McVey_Cason
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declares an intger to store a favorite number
+            int favoriteNumber;
+            // Booleans initialized to false for jumping and running
+            bool isJumping = false, isRunning = false;
+            // Declared random float variable
+            float myFloat;
+            // Assigning the favoriteNumber to 17
+            favoriteNumber = 17;
+            // Assigning myFloat to 55.55
+            myFloat = 55.55f;
+            // Initializing constant finalGrade to 99.9
+            const double finalGrade = 99.9;
+
+            // Prints all variables to console
+            Console.WriteLine("Favorite #: " + favoriteNumber);
+            Console.WriteLine("Jumping: " + isJumping);
+            Console.WriteLine("Running: " + isRunning);
+            Console.WriteLine("Random Floating #: " + myFloat);
+            Console.WriteLine("Final Grade: " + finalGrade);
+        }
+    }
+}
+
